7a5ec09da3e9e2d07c0bd3d0b8c85ea0e13f52bf
Tushar Khan <tushar.54k@gmail.com>
2019-08-04 21:20:48 -0400

    Moved module to scrapers file

--- scrapers/billboard_scraper.py
+++ scrapers/billboard_scraper.py
@@ -4,48 +4,52 @@ Please note that many web scraping may be against the terms of use of some websi
 web scraping, and may even block
 """
 
-import requests, datetime, time, csv
-from bs4 import BeautifulSoup, element
+import requests, datetime, time, csv, sys
+from bs4 import BeautifulSoup
 
 datasets_folder = 'datasets'
 
-def scrape_billboard_hot_100(min_date: datetime.date, max_date=datetime.date.today(), data_file='billboard_hot_100.csv'):
+def scrape_hot_100(min_date:tuple, max_date:tuple=None, data_file='billboard_hot_100.csv'):
     """
     Extracts data from the Billboard Hot 100 chart for a given range of dates.
 
     Parameters:
-    min_date (datetime.date)    - The first week of the Billboard Hot 100 to extract data (format yyyy-mm-dd).
-                                    ~ First week of Billboard Hot 100 was 1958-08-04.
-    max_date (datetime.date)    - The last week of the Billboard Hot 100 to extract data (format yyyy-mm-dd).
-                                    ~ Most current week by default.
-    data_file (str)             - A .csv file name to store the extracted data; will be overwritten if it already exists
-                                    ~ 'billboard_hot_100.csv' by default.
+    min_date (tuple)        - The first week of the Billboard Hot 100 to extract data, formatted (mm, dd, yyyy).
+                                ~ First week of Billboard Hot 100 was 1958-08-04
+    max_date (tuple)        - The last week of the Billboard Hot 100 to extract data, formatted (mm, dd, yyyy).
+                                ~ Most current week by default
+    data_file (str)         - A .csv file name to store the extracted data; will be overwritten if it already exists.
+                                ~ If the file does not end in '.csv', it will be appended to the file name
+                                ~ 'billboard_hot_100.csv' by default
 
     Dataset Features:
-    Position (int)              - The rank of the song in the current week
-    Title (str)                 - The song title
-    Artist (str)                - The song artists
-    Last Week (int)             - The rank of the song in the previous week
-                                    ~ Empty if the song was not on the Billboard Hot 100 the previous week
-    Peak Position (int)         - The highest rank the song has reached on the Billboard Hot 100 until and including the current week
-    Weeks on Chart (int)        - The number of weeks the song has been on the Billboard Hot 100 until and including the current week
-    Chart Date (str)            - The date of the week the datapoint was collected from in the format mm-dd-yyyy
+    Position (int)          - The rank of the song in the current week
+    Title (str)             - The song title
+    Artist (str)            - The song artists
+    Last Week (int)         - The rank of the song in the previous week
+                                ~ Empty if the song was not on the Billboard Hot 100 the previous week
+    Peak Position (int)     - The highest rank the song has reached on the Billboard Hot 100 until and including the current week
+    Weeks on Chart (int)    - The number of weeks the song has been on the Billboard Hot 100 until and including the current week
+    Chart Date (str)        - The date of the week the datapoint was collected from in the format mm-dd-yyyy
     """
 
     base_url = 'https://www.billboard.com/charts/hot-100'
     features = ['Position', 'Title', 'Artist', 'Last Week', 'Peak Position', 'Weeks on Chart', 'Chart Date']
     total_time, charts_fetched = 0, 0
 
+    # Date range
+    min_date = datetime.date(min_date[2], min_date[0], min_date[1])
+    date = datetime.date.today() if not max_date else datetime.date(max_date[2], max_date[0], max_date[1])
+    date += datetime.timedelta((12 - date.weekday()) % 7) # Gets next Saturday
+    if date < min_date: return
+
+    if data_file[-4:] != '.csv': data_file += '.csv'
     global datasets_folder
     with open(f'{datasets_folder}/{data_file}', mode='w', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, features)
         writer.writeheader()
 
-        # Date range
-        date = max_date
-        date += datetime.timedelta((12 - date.weekday()) % 7) # Gets next Saturday
         week_delta = datetime.timedelta(weeks=1)
-
         while date >= min_date:
             # Collect program metadata
             charts_fetched += 1
@@ -53,13 +57,14 @@ def scrape_billboard_hot_100(min_date: datetime.date, max_date=datetime.date.tod
 
             # Get current Billboard Hot 100 page
             print('Fetching chart for week of', date.strftime('%m-%d-%Y'), '...', end=' ')
+            sys.stdout.flush()
             try:
-                page = requests.get('{url}/{date}'.format(url=base_url, date=date.strftime('%Y-%m-%d')), stream=True)
+                page = requests.get('{url}/{date}'.format(url=base_url, date=date.strftime('%Y-%m-%d')))
                 if not page:
                     print('an error has occurred!')
                     continue
             except requests.exceptions.ConnectionError:
-                print('The connection was unexpectadly closed.')
+                print('The connection was unexpectedly closed.')
                 raise
             soup = BeautifulSoup(page.text, 'lxml')
 
@@ -91,7 +96,6 @@ def scrape_billboard_hot_100(min_date: datetime.date, max_date=datetime.date.tod
                 song_data.append(song_info)
 
             # Append data to CSV
-            writer = csv.DictWriter(csv_file, features)
             writer.writerows(song_data)
 
             # Update program metadata
@@ -100,11 +104,8 @@ def scrape_billboard_hot_100(min_date: datetime.date, max_date=datetime.date.tod
             print('done in', round(time_interval, 5), 'seconds')
             time.sleep(2)
 
-    print('DONE; fetched', charts_fetched, 'week{s} of Billboard Hot 100 data in'.format(s='' if charts_fetched == 1 else 's'), round(total_time, 5), 'seconds')
+    print('DONE; fetched', charts_fetched, 'week{s} of Billboard Hot 100 data in'.format(s='' if charts_fetched == 1 else 's'), round(total_time, 5), 'seconds.')
+    return data_file
 
 if __name__ == '__main__':
-    scrape_billboard_hot_100(min_date=datetime.date(2015, 1, 1), max_date=datetime.date(2015, 12, 31), data_file='2015-billboard_hot_100.csv')
-    scrape_billboard_hot_100(min_date=datetime.date(2016, 1, 1), max_date=datetime.date(2016, 12, 31), data_file='2016-billboard_hot_100.csv')
-    scrape_billboard_hot_100(min_date=datetime.date(2017, 1, 1), max_date=datetime.date(2017, 12, 31), data_file='2017-billboard_hot_100.csv')
-    scrape_billboard_hot_100(min_date=datetime.date(2018, 1, 1), max_date=datetime.date(2018, 12, 31), data_file='2018-billboard_hot_100.csv')
-    scrape_billboard_hot_100(min_date=datetime.date(2019, 1, 1), max_date=datetime.date(2019, 12, 31), data_file='2019-billboard_hot_100.csv')
+    pass
